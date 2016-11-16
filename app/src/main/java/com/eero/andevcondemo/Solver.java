c6d83da540fc78ebc41f8f23849223e30580250b
Jordan Jozwiak <jordanjoz1@gmail.com>
2016-11-15 21:26:47 -0800

    added some bad code

---
+++ app/src/main/java/com/eero/andevcondemo/Solver.java
@@ -0,0 +1,8 @@
+package com.eero.andevcondemo;
+
+/**
+ * Created by jordan on 11/15/16.
+ */
+
+public class Solver {
+}
---
+++ app/src/test/java/com/eero/andevcondemo/SolverTest.java
@@ -0,0 +1,16 @@
+package com.eero.andevcondemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jordan on 11/15/16.
+ */
+public class SolverTest {
+    @Test
+    public void calcFib() throws Exception {
+
+    }
+
+}
